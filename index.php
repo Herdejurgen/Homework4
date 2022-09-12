dbb03da4af8bc72404795b5d2832e92367c25de4
Herdejurgen <98052580+Herdejurgen@users.noreply.github.com>
2022-09-12 14:31:39 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
   </head>
   
   <body>
+    <table class = "table">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+      </tr>
     <?php
       $servername = "localhost";
       $username = "Homework3";
@@ -35,6 +40,7 @@
       }
       $conn->close();
   ?>
+    </table>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
     
