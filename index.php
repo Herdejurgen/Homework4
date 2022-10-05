3d94b43269adb40c86294a57d27ef7cc3757f593
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-10-05 11:25:52 -0500

    reformat

--- index.php
+++ index.php
@@ -16,40 +16,40 @@
       </tr>
     </thead>
     <tbody>
-    <?php
-      $servername = "localhost";
-      $username = "herdeju1_homework3";
-      $password = "{k)#n$3fgsGn";
-      $dbname = "herdeju1_Homework3";
-
-      // Create connection
-      $conn = new mysqli($servername, $username, $password, $dbname);
-
-      // Check connection
-      if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
-      echo "Connected successfully";
-
-      $sql = "SELECT instructor_id, instructor_name FROM Instructor";
-      $result = $conn->query($sql);
-
-      if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-        ?>
-        <tr>
-            <td><?echo $row["instructor_id"]?></td>
-            <td><?echo $row["instructor_name"]?></td>
-        </tr>
         <?php
-        }
-      } else {
-        echo "0 results";
-      }
-      $conn->close();
-  ?>
-  </tbody>
+          $servername = "localhost";
+          $username = "herdeju1_homework3";
+          $password = "{k)#n$3fgsGn";
+          $dbname = "herdeju1_Homework3";
+
+          // Create connection
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          // Check connection
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+          echo "Connected successfully";
+
+          $sql = "SELECT instructor_id, instructor_name FROM Instructor";
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+            ?>
+            <tr>
+                <td><?echo $row["instructor_id"]?></td>
+                <td><?echo $row["instructor_name"]?></td>
+            </tr>
+            <?php
+            }
+          } else {
+            echo "0 results";
+          }
+          $conn->close();
+        ?>
+    </tbody>
     </table>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
